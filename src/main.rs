9ae5a346eb91d878cefc3fd9ccaba4c5f16fd3d8
Ashutosh-Panda-340 <pandaashutosh340@gmail.com>
2025-03-15 22:55:27 +0530

    did some basic printing into the VGA buffer .. let's see

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 #![no_std]
 #![no_main] // disabling all rust level entry points 
 // #![feature(asm)] // for inline assembly -- has been a feature since a while in the nightly versions
@@ -6,6 +7,7 @@
 // gonna have the std
 //
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -16,22 +18,7 @@ static HELLO: &[u8] = b"Hello World";
 
 #[no_mangle] // read about this a bit
 pub extern "C" fn _start() -> ! {
-    // entry point should not return instead do the exit system call
-    // since the linker looks for a start point 
-    //
-    let vga_buffer = 0xb8000 as *mut u8;  // the starting position of the VGA buffer is fixed!
-                                          
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe { // raw pointer operations can lead to memory corruptions is used incorrectly!
-                 // ----- so use this within the unsafe block!
-
-            *vga_buffer.offset(i as isize * 2) = byte;  // writes the character byte into the buffer
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // write the color byte right after the character byte
-            
-            // the multiplication by 2 is needed cuz each character cell is 2 bytes 
-
-        }
-    }
+    vga_buffer::print_something();
 
     loop{}
 }
---
+++ src/vga_buffer.rs
@@ -0,0 +1,148 @@
+/// helper module to interact with the VGA buffer. Unsafe is abstracted here.
+// src/vga_buffer.rs
+
+#[allow(dead_code)]
+
+// gotta make some colors .. the bright bit combines with the normal bits to form the bright colors
+// in the VGA buffer 
+//
+/// contains the different colors possible through the VGA buffer
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]  // enable copy semantics and make it printable and comparable
+#[repr(u8)] // each of the colors is represented by an 8-bit unsigned integer
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+
+/// to represent a full color code that specifies the foreground and background color
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[repr(transparent)]  // tells that it should have the exact same memory layout as its fields i.e. u8
+struct ColorCode(u8);  // see the newtype idiom
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        // Higher 4 bits: background
+        // Lower  4 bits: foreground
+        ColorCode((background as u8) << 4 | (foreground as u8))
+        // background is shifted by 4 bits .. placing it in the higher nibble
+    }
+}
+
+
+/// to represent a screen character in the VGA text buffer
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)] // so that the struct fields are laid out in order as in C .. not in rust by default
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+// the VGA text buffer is a 2D array that has 25 rows and 80 columns
+/// the VGA screen displays 25 lines of text
+const BUFFER_HEIGHT: usize = 25;
+/// each VGA line can show 80 characters
+const BUFFER_WIDTH: usize = 80;
+
+/// to represent the VGA Buffer -- 2D array <br>
+/// It is a contiguous block of memory starting at 0xb8000
+#[repr(transparent)]
+struct Buffer {
+    /// 2D array to represent characters
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT], 
+}
+
+/// writer type to write into the screen [VGA]
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    /// **mutable** reference to the buffer <br> -- reference passed since you **can't own/create** the VGA
+    buffer: &'static mut Buffer, // guaranteed to be valid for the entire duration of the program
+}
+
+
+impl Writer {
+
+    /// writes a bytes to the VGA buffer <br>
+    /// parameters: <br>
+    /// byte: u8  -- the byte you want to write -- 8 bits
+    pub fn write_byte(&mut self, byte: u8)
+    {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT -1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code: color_code,
+                };
+                self.column_position += 1; // since you wrote one byte move to the next column
+            }
+        }
+    }
+
+    /// prints a new line to the VGA buffer
+    pub fn new_line(&mut self)
+    {
+        // TODO
+    }
+
+    /// write a string into the VGA buffer <br>
+    /// parameters: <br>
+    /// s: &str
+    ///
+    /// <br> prints a '■' for unprintable bytes
+    pub fn write_string(&mut self, s: &str)
+    {
+        for byte in s.bytes() {
+            // rust strings are UTF-8 by default so they might contain some unsupported chars by
+            // the vga_buffer
+            match byte {
+                // check for printable ascii_character or new_line
+                // 0x20 is 32 in decimal for ' '
+                // 0x7e is 126 in decimal for '~'
+                // they denote the printable ASCII range
+                // range inclusive notation -- remember it
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of the of the printable ASCII range
+                _ => self.write_byte(0xfe), // print a ■ for unprintable bytes 
+            }
+        }
+    }
+}
+
+pub fn print_something() 
+{
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Blue),
+        buffer: unsafe {
+            &mut *(0xb8000 as *mut Buffer)
+        },
+    };
+
+    writer.write_byte(b'H');
+    writer.write_string("ello ");
+    writer.write_string("Wörld!");
+}
